85de48989febae18d35a93a179d9ba1fd06250b6
EntropyNetwork <117935093+EntropyNetwork@users.noreply.github.com>
2023-08-09 10:23:13 -0500

    Update main.py
    Add some argparsing

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import argparse
+passphrase = """the robots are as devious as you please, controller."""
+
 def main():
+  parser = argparse.ArgumentParser(
+                    prog='Boilerplater',
+                    description='Generate random terms and conditions per click',
+                    epilog='By using this program, you have agreed to my terms and conditions.')
+  
   ultimate_contract = "By using this website, you agree to the following terms and conditions:"
